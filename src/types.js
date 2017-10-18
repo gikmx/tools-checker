6c9d7229e80ab2788e647f5a10bdfac3b9748eca
Héctor Menéndez <hmenendez@techlap.local>
2017-10-18 04:32:18 -0500

    Updated jsdoc for types.

--- src/types.js
+++ src/types.js
@@ -5,59 +5,61 @@ const primitives = Object
     .join('|');
 
 /**
- * @module __types
- * @memberof Tools
+ * An expected argument for props was invalid or not provided.
+ * @typedef {Error}
+ * @memberof Types
  */
+export const CheckerPropParamError = {
+
+    name: 'CheckerPropParamError',
+    message: 'Invalid parameter «%s». Expecting {%s}, got "%s"',
+};
+
+/**
+ * A definition prop was sent, but it was invalid.
+ * @typedef {Error}
+ * @memberof Types
+ */
+export const CheckerPropDefError = {
+    name: 'CheckerPropDefError',
+    message: 'Invalid def «%s». Expecting {%s}, got "%s"',
+    // message: 'Expecting an {Object|string} for def: ',
+};
+
+/**
+ * The specified type is not a supported primitive.
+ * @typedef {Error}
+ * @memberof Types
+ */
+export const CheckerPropDefTypeError = {
+    name: 'CheckerPropDefTypeError',
+    message: `Invalid type on def «%s». Expecting [${primitives}], got "%s"`,
+};
+
+/**
+ * A required property was not found in subject.
+ * @typedef {Error}
+ * @memberof Types
+ */
+export const CheckerPropReqError = {
+    name: 'CheckerPropReqError',
+    message: 'Missing required property «%s».',
+};
+
+/**
+ * A property didn't have the correct type.
+ * @typedef {Error}
+ * @memberof Types
+ */
+export const CheckerPropTypeError = {
+    name: 'CheckerPropTypeError',
+    message: 'Invalid property «%s». Expecting {%s} got "%s"',
+};
+
 export default {
-
-
-    /**
-     * @typedef {Error}
-     * An expected argument for props was invalid or not provided.
-     * @memberof Tools.__types
-     */
-    CheckerPropParamError: {
-        name: 'CheckerPropParamError',
-        message: 'Invalid parameter «%s». Expecting {%s}, got "%s"',
-    },
-    /**
-     * A definition prop was sent, but it was invalid.
-     * @typedef {Error}
-     * @memberof Tools.__types
-     */
-    CheckerPropDefError: {
-        name: 'CheckerPropDefError',
-        message: 'Invalid def «%s». Expecting {%s}, got "%s"',
-        // message: 'Expecting an {Object|string} for def: ',
-    },
-
-    /**
-     * The specified type is not a supported primitive.
-     * @typedef {Error}
-     * @memberof Tools.__types
-     */
-    CheckerPropDefTypeError: {
-        name: 'CheckerPropDefTypeError',
-        message: `Invalid type on def «%s». Expecting [${primitives}], got "%s"`,
-    },
-
-    /**
-     * A required property was not found in subject.
-     * @typedef {Error}
-     * @memberof Tools.__types
-     */
-    CheckerPropReqError: {
-        name: 'CheckerPropReqError',
-        message: 'Missing required property «%s».',
-    },
-
-    /**
-     * A property didn't have the correct type.
-     * @typedef {Error}
-     * @memberof Tools.__types
-     */
-    CheckerPropTypeError: {
-        name: 'CheckerPropTypeError',
-        message: 'Invalid property «%s». Expecting {%s} got "%s"',
-    },
+    CheckerPropParamError,
+    CheckerPropDefTypeError,
+    CheckerPropDefError,
+    CheckerPropTypeError,
+    CheckerPropReqError,
 };
